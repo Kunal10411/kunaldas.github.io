e0334319543fd36d8e5af2ec14f83e3cd3febfb4
Kunal <Kunaldas1423@gmail.com>
2024-12-16 11:43:09 +0530

    Revamped

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+document.addEventListener("DOMContentLoaded", function() {
+  const loader = document.querySelector(".loader");
+  const mainContent = document.getElementById("main-content");
+
+  // Initially hide the main content
+  mainContent.style.visibility = "hidden";
+
+  // Show loading animation for 2 seconds
+  setTimeout(function() {
+      loader.style.display = "none";
+      mainContent.style.visibility = "visible";
+      mainContent.style.opacity = "1"; // Fade in the main content
+  }, 2000);
+});
