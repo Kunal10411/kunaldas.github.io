055acb7f92bf29651694785a352743b0cb798368
Kunal <Kunaldas1423@gmail.com>
2024-12-16 15:52:00 +0530

    Revamped

--- script.js
+++ script.js
@@ -11,8 +11,4 @@ document.addEventListener("DOMContentLoaded", function() {
       mainContent.style.visibility = "visible";
       mainContent.style.opacity = "1"; // Fade in the main content
   }, 2000);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> d77b85335ab02c314c96268b25752f8e210b6177
